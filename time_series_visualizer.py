035843f654b86a9b25430bc143dbba83209b88cd
SSB-11 <shelles.borges@gmail.com>
2024-09-25 20:21:40 +0000

    feat: implement time series line plot

--- time_series_visualizer.py
+++ time_series_visualizer.py
@@ -5,18 +5,23 @@ from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 
 # Import data (Make sure to parse dates. Consider setting index column to 'date'.)
-df = None
+df = pd.read_csv('fcc-forum-pageviews.csv', index_col=0, parse_dates=['date'])
 
 # Clean data
-df = None
+df = df.loc[
+    (df['value'] >= df['value'].quantile(0.025)) & 
+    (df['value'] <= df['value'].quantile(0.975))
+]
 
 
 def draw_line_plot():
     # Draw line plot
-
-
-
-
+    plt.figure(figsize=(22, 6))
+    plt.title('Daily freeCodeCamp Forum Page Views 5/2016-12/2019', fontsize=16)
+    plt.xlabel('Date', fontsize=12)
+    plt.ylabel('Page Views', fontsize=12)
+    plt.plot(df, color='blue', linewidth=2)
+    fig = plt.gcf()
 
     # Save image and return fig (don't change this part)
     fig.savefig('line_plot.png')
